33c34821a1de4827b69182d2cb24d6144c46ce64
Mack Yuen <myuen@mail.ubc.ca>
2022-06-27 11:04:56 -0700

    - Rename variables - Rewrite plot function. Title add after plot

--- analysis/03-2_TBB-expression-plots.R
+++ analysis/03-2_TBB-expression-plots.R
@@ -4,7 +4,7 @@ library(stringr)
 
 #####
 
-TBB_exp_plot <- function(stats, title){
+TBB_exp_plot <- function(stats){
   
   ggplot(stats, aes(y = enzyme, x = logFC)) +   
     geom_point(size = 2, colour = "black") + 
@@ -15,7 +15,6 @@ TBB_exp_plot <- function(stats, title){
     
     geom_vline(xintercept = c(-2, 2), colour = "black", linetype = 2) + 
     
-    ggtitle(title) +
     labs(caption = "p-value <= 0.05") +
     xlab(expression(paste("log"[2],"FC"))) +
     ylab("") +
@@ -37,28 +36,33 @@ str(UBC_dea_stats)
 
 
 # Read putative TBB CDS IDs from functional domain scan with hmmscan and rpsblast.
-UBC_tbb_orthologs <- 
-  read.table("data/targeted-pathway-annotation/01-Terpenoid_backbone_biosynthesis/UBC_tbb_orthologs.txt",
+UBC_TBB_orthologs <- 
+  read.table("data/targeted-pathway-annotation/01-Terpenoid_backbone_biosynthesis/UBC_TBB_orthologs.txt",
              stringsAsFactors = FALSE, header = TRUE)
-str(UBC_tbb_orthologs)
+
+str(UBC_TBB_orthologs)
 # 'data.frame':	60 obs. of  4 variables:
 
 
-UBC_tbb_ortholog_stats <- 
-  inner_join(UBC_dea_stats, UBC_tbb_orthologs)
+UBC_TBB_ortholog_stats <- 
+  inner_join(UBC_dea_stats, UBC_TBB_orthologs)
 # Joining, by = "cds"
 
-str(UBC_tbb_ortholog_stats)
+str(UBC_TBB_ortholog_stats)
 # 'data.frame':	54 obs. of  6 variables:
 
-UBC_tbb_ortholog_stats$pathway <- 
-  factor(UBC_tbb_ortholog_stats$pathway, 
+UBC_TBB_ortholog_stats$pathway <- 
+  factor(UBC_TBB_ortholog_stats$pathway, 
          levels = c("MEP", "MEV", "C1020"))
 
-(UBC_tbb_plot <-
-  TBB_exp_plot(UBC_tbb_ortholog_stats, "UBC Terpenoid Backbone Biosynthesis"))
+UBC_TBB_plot <-
+  TBB_exp_plot(UBC_TBB_ortholog_stats)
 
-ggsave("results/figures/UBC_TBB_expression.svg", plot = UBC_tbb_plot)
+(UBC_TBB_plot <-
+    UBC_TBB_plot + 
+      ggtitle("UBC Terpenoid Backbone Biosynthesis"))
+
+ggsave("results/figures/UBC_TBB_expression.svg", plot = UBC_TBB_plot)
 
 #####
 
@@ -68,27 +72,30 @@ str(JGI_dea_stats)
 # 'data.frame':	84192 obs. of  4 variables:
 
 
-JGI_tbb_orthologs <- 
-  read.table("data/targeted-pathway-annotation/01-Terpenoid_backbone_biosynthesis/JGI_tbb_orthologs.txt",
+JGI_TBB_orthologs <- 
+  read.table("data/targeted-pathway-annotation/01-Terpenoid_backbone_biosynthesis/JGI_TBB_orthologs.txt",
              stringsAsFactors = FALSE, header = TRUE)
-str(JGI_tbb_orthologs)
+str(JGI_TBB_orthologs)
 # 'data.frame':	90 obs. of  4 variables:
 
 
-JGI_tbb_ortholog_stats <- 
-  inner_join(JGI_dea_stats, JGI_tbb_orthologs)
+JGI_TBB_ortholog_stats <- 
+  inner_join(JGI_dea_stats, JGI_TBB_orthologs)
 # Joining, by = "cds"
 
-str(JGI_tbb_ortholog_stats)
+str(JGI_TBB_ortholog_stats)
 # 'data.frame':	128 obs. of  7 variables:
 
 
-JGI_tbb_ortholog_stats$pathway <- 
-  factor(JGI_tbb_ortholog_stats$pathway, 
+JGI_TBB_ortholog_stats$pathway <- 
+  factor(JGI_TBB_ortholog_stats$pathway, 
          levels = c("MEP", "MEV", "C1020"))
 
 
-(JGI_tbb_plot <-
-    TBB_exp_plot(JGI_tbb_ortholog_stats, "JGI Terpenoid Backbone Biosynthesis"))
+JGI_TBB_plot <- 
+  TBB_exp_plot(JGI_TBB_ortholog_stats)
 
-ggsave("results/figures/JGI_TBB_expression.svg", plot = JGI_tbb_plot)
+(JGI_TBB_plot <- 
+    JGI_TBB_plot + ggtitle("JGI Terpenoid Backbone Biosynthesis"))
+
+ggsave("results/figures/JGI_TBB_expression.svg", plot = JGI_TBB_plot)
