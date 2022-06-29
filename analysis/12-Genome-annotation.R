959313892e714610f4c7bd9221bc387ced3fa9ac
Mack Yuen <myuen@mail.ubc.ca>
2022-06-29 16:43:35 -0700

    Redundant to read statistics since all BLAST results is from DE contigs

--- analysis/12-Genome-annotation.R
+++ analysis/12-Genome-annotation.R
@@ -26,20 +26,11 @@ blast_header <-
 
 #####
 
-# Read the statistics from UBC DEA
-ubc_sigDE <- read.table("results/ubc_dea_sig_results.txt", 
-                        header = TRUE, stringsAsFactors = FALSE)
-
-colnames(ubc_sigDE)[1] <- "ctg_cds"
-
-str(ubc_sigDE)
-
-
 # Read BLAST results
 ubc_genome_blast <- 
   read.delim("data/ubc_dea_sig.blastWRCv3Annotation.txt", sep = "\t",
              stringsAsFactors = FALSE, col.names = blast_header) %>% 
-  select(-c("staxid", "sskingdom", "sscinames", "scomnames"))
+  select(ctg_cds, mRNA, ppos, qcovs, salltitles)
 
 str(ubc_genome_blast)
 # 'data.frame':	13669 obs. of  16 variables:
@@ -49,56 +40,38 @@ ubc_genome_blast <- ubc_genome_blast %>%
   filter(ppos >= 95 & qcovs >= 50)
 
 str(ubc_genome_blast)
-# 'data.frame':	2150 obs. of  16 variables:
+# 'data.frame':	2150 obs. of  5 variables:
 
 
 #Take the top best hit from BLAST
-ubc_genome_blast <- ubc_genome_blast %>% 
+ubc_top_genome_blast <- ubc_genome_blast %>% 
   group_by(ctg_cds) %>% 
-  arrange(length, ppos) %>% 
+  arrange(ppos, qcovs) %>% 
   slice_head(n = 1) %>% 
   ungroup()
 
-ubc_genome_blast$locus <- 
-  str_replace(ubc_genome_blast$mRNA, "\\.\\d$", "")
+ubc_top_genome_blast$locus <- 
+  str_replace(ubc_top_genome_blast$mRNA, "\\.\\d$", "")
 
-ubc_genome_blast$scaffold <- ubc_genome_blast$locus %>% 
+ubc_top_genome_blast$scaffold <- ubc_top_genome_blast$locus %>% 
   str_replace("Thupl.", "") %>% str_replace("s\\d+", "")
 
-ubc_genome_blast <- ubc_genome_blast %>%
+ubc_top_genome_blast <- ubc_top_genome_blast %>%
   select(ctg_cds, scaffold, locus)
 
-UBC_annots <- left_join(ubc_sigDE, ubc_genome_blast, by = "ctg_cds")
-
-table(is.na(UBC_annots$scaffold))
-# FALSE  TRUE 
-#  1610   789 
-### 1610 contigs have no genome annotation
-
-
 #Add gene function
-UBC_annots <- left_join(UBC_annots, gene_function, by = "locus")
+UBC_annots <- left_join(ubc_top_genome_blast, gene_function, by = "locus")
 
 write.table(UBC_annots, "results/ubc_dea_sig_genome-annotated.txt",
             sep = "\t", quote = FALSE, row.names = FALSE)
 
 #####
 
-# Read the statistics from UBC DEA
-jgi_sigDE <- read.table("results/jgi_dea_sig_results.txt", 
-                        header = TRUE, stringsAsFactors = FALSE)
-
-colnames(jgi_sigDE)[1] <- "ctg_cds"
-
-str(jgi_sigDE)
-# 'data.frame':	22404 obs. of  4 variables:
-
-
 # Read BLAST results
 jgi_genome_blast <- 
   read.delim("data/jgi_dea_sig.blastWRCv3Annotation.txt", sep = "\t", 
              stringsAsFactors = FALSE, col.names = blast_header) %>% 
-  select(-c("staxid", "sskingdom", "sscinames", "scomnames"))
+  select(ctg_cds, mRNA, ppos, qcovs, salltitles)
 
 str(jgi_genome_blast)
 # 'data.frame':	103175 obs. of  16 variables:
@@ -107,41 +80,29 @@ jgi_genome_blast <- jgi_genome_blast %>%
   filter(ppos >= 95 & qcovs >= 50)
 
 str(jgi_genome_blast)
-# 'data.frame':	23479 obs. of  16 variables:
+# 'data.frame':	23479 obs. of  5 variables:
 
 
 #Take the top best hit from BLAST
-jgi_genome_blast <- jgi_genome_blast %>% 
+jgi_top_genome_blast <- jgi_genome_blast %>% 
   group_by(ctg_cds) %>% 
-  arrange(length, ppos) %>% 
+  arrange(ppos, qcovs) %>% 
   slice_head(n = 1) %>% 
   ungroup()
 
 
-jgi_genome_blast$locus <- 
-  str_replace(jgi_genome_blast$mRNA, "\\.\\d$", "")
+jgi_top_genome_blast$locus <- 
+  str_replace(jgi_top_genome_blast$mRNA, "\\.\\d$", "")
 
 jgi_genome_blast$scaffold <- jgi_genome_blast$locus %>% 
   str_replace("Thupl.", "") %>% str_replace("s\\d+", "")
 
-jgi_genome_blast <- jgi_genome_blast %>%
+jgi_top_genome_blast <- jgi_top_genome_blast %>%
   select(ctg_cds, scaffold, locus)
 
-JGI_annots <- left_join(jgi_sigDE, jgi_genome_blast, by = "ctg_cds")
-
-table(JGI_annots$focus)
-# gMature_glMature   gYoung_glYoung 
-#              669            21735 
-
-table(is.na(JGI_annots$scaffold), JGI_annots$focus)
-#         gMature_glMature gYoung_glYoung
-#   FALSE              357          18118
-#   TRUE               312           3617
-### 3617 have from Young Gland vs Young Glandless have no genome annotation
-### 357 have from Young Gland vs Young Glandless have no genome annotation
-
 #Add gene function
-JGI_annots <- left_join(JGI_annots, gene_function, by = "locus")
+JGI_annots <- 
+  left_join(jgi_top_genome_blast, gene_function, by = "locus")
 
 write.table(JGI_annots, "results/jgi_dea_sig_genome-annotated.txt",
             sep = "\t", quote = FALSE, row.names = FALSE)
