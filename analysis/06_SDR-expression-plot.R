7c80684f6cec01fb4ecf881b942dcf4e1bcb8c6c
Mack Yuen <myuen@mail.ubc.ca>
2022-07-18 10:21:22 -0700

    - Fixed a line where cloned SDR were left out. - Will focus on UBC dataset.  Line on JGI dataset plot are commented out.

--- analysis/06_SDR-expression-plot.R
+++ analysis/06_SDR-expression-plot.R
@@ -60,14 +60,15 @@ length(UBC_cloned_SDRs)
 # [1] 5
 
 
-UBC_SDRs <- data.frame(rownames = c(unique(UBC_putative_SDRs, UBC_cloned_SDRs)))
+UBC_SDRs <- 
+  data.frame(rownames = c(union(UBC_putative_SDRs, UBC_cloned_SDRs)))
                        
 UBC_SDRs$type <- "Putative"
 
 colnames(UBC_SDRs)[1] <- 'cds'
 
 str(UBC_SDRs)
-# 'data.frame':	357 obs. of  2 variables:
+# 'data.frame':	359 obs. of  2 variables:
 
 
 UBC_SDRs[UBC_SDRs$cds %in% UBC_cloned_SDRs,][2] <- "Cloned"
@@ -95,66 +96,66 @@ ggsave("results/figures/UBC_SDR_expression.svg", plot = UBC_SDR_exp_plot)
 
 
 #####
-
-JGI_dea_stats <- read.delim("results/jgi_dea_results.txt",
-                            header = TRUE, stringsAsFactors = FALSE)
-str(JGI_dea_stats)
-# 'data.frame':	84192 obs. of  4 variables:
-
-
-JGI_putative_SDRs <- 
-  scan("data/targeted-pathway-annotation/04-SDR_ADH/putative_SDR.JGI_assembly.size_filtered.cdsID.txt",
-       what = "character")
-
-JGI_putative_SDRs <- unique(JGI_putative_SDRs)
-
-length(JGI_putative_SDRs)
-# [1] 383
-
-
-JGI_cloned_SDRs <- 
-  scan("data/targeted-pathway-annotation/04-SDR_ADH/cloned_SDR_JGI_cdsID.txt", 
-       what = "character")
-
-JGI_cloned_SDRs <- unique(JGI_cloned_SDRs)
-
-length(JGI_cloned_SDRs)
-# [1] 3
-
-
-JGI_SDRs <- data.frame(rownames = c(unique(JGI_putative_SDRs, JGI_cloned_SDRs)))
-
-JGI_SDRs$type <- "Putative"
-
-colnames(JGI_SDRs)[1] <- 'cds'
-
-str(JGI_SDRs)
-# 'data.frame':	383 obs. of  2 variables:
-
-
-JGI_SDRs[JGI_SDRs$cds %in% JGI_cloned_SDRs,][2] <- "Cloned"
-
-str(JGI_SDRs)
-# 'data.frame':	699 obs. of  2 variables:
-
-
-JGI_SDR_stats <- inner_join(JGI_dea_stats, JGI_SDRs)
-# Joining, by = "cds"
-
-str(JGI_SDR_stats)
-# 'data.frame':	114 obs. of  5 variables:
-
-JGI_SDR_stats$type <- factor(JGI_SDR_stats$type,
-                             levels = c("Putative", "Cloned"))
-
-JGI_SDR_stats$focus <- factor(JGI_SDR_stats$focus,
-                             levels = c("gYoung_glYoung", "gMature_glMature"))
-
-JGI_SDR_exp_plot <- SDR_exp_plot(JGI_SDR_stats)
-
-(JGI_SDR_exp_plot <- JGI_SDR_exp_plot +
-    ggtitle("JGI Short Chained Alcohol Dehydrogenase (SDR) Expression", 
-          subtitle = "Size filtered between 250 to 350 a.a") +
-    facet_wrap(~ focus, nrow = 2, strip.position = "right"))
-
-ggsave("results/figures/JGI_SDR_expression.svg", plot = JGI_SDR_exp_plot)
+# 
+# JGI_dea_stats <- read.delim("results/jgi_dea_results.txt",
+#                             header = TRUE, stringsAsFactors = FALSE)
+# str(JGI_dea_stats)
+# # 'data.frame':	84192 obs. of  4 variables:
+# 
+# 
+# JGI_putative_SDRs <- 
+#   scan("data/targeted-pathway-annotation/04-SDR_ADH/putative_SDR.JGI_assembly.size_filtered.cdsID.txt",
+#        what = "character")
+# 
+# JGI_putative_SDRs <- unique(JGI_putative_SDRs)
+# 
+# length(JGI_putative_SDRs)
+# # [1] 383
+# 
+# 
+# JGI_cloned_SDRs <- 
+#   scan("data/targeted-pathway-annotation/04-SDR_ADH/cloned_SDR_JGI_cdsID.txt", 
+#        what = "character")
+# 
+# JGI_cloned_SDRs <- unique(JGI_cloned_SDRs)
+# 
+# length(JGI_cloned_SDRs)
+# # [1] 3
+# 
+# 
+# JGI_SDRs <- data.frame(rownames = c(unique(JGI_putative_SDRs, JGI_cloned_SDRs)))
+# 
+# JGI_SDRs$type <- "Putative"
+# 
+# colnames(JGI_SDRs)[1] <- 'cds'
+# 
+# str(JGI_SDRs)
+# # 'data.frame':	383 obs. of  2 variables:
+# 
+# 
+# JGI_SDRs[JGI_SDRs$cds %in% JGI_cloned_SDRs,][2] <- "Cloned"
+# 
+# str(JGI_SDRs)
+# # 'data.frame':	699 obs. of  2 variables:
+# 
+# 
+# JGI_SDR_stats <- inner_join(JGI_dea_stats, JGI_SDRs)
+# # Joining, by = "cds"
+# 
+# str(JGI_SDR_stats)
+# # 'data.frame':	114 obs. of  5 variables:
+# 
+# JGI_SDR_stats$type <- factor(JGI_SDR_stats$type,
+#                              levels = c("Putative", "Cloned"))
+# 
+# JGI_SDR_stats$focus <- factor(JGI_SDR_stats$focus,
+#                              levels = c("gYoung_glYoung", "gMature_glMature"))
+# 
+# JGI_SDR_exp_plot <- SDR_exp_plot(JGI_SDR_stats)
+# 
+# (JGI_SDR_exp_plot <- JGI_SDR_exp_plot +
+#     ggtitle("JGI Short Chained Alcohol Dehydrogenase (SDR) Expression", 
+#           subtitle = "Size filtered between 250 to 350 a.a") +
+#     facet_wrap(~ focus, nrow = 2, strip.position = "right"))
+# 
+# ggsave("results/figures/JGI_SDR_expression.svg", plot = JGI_SDR_exp_plot)
